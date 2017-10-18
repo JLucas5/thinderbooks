90147bdfcc6a1223397e10b07d3d825b7a64a3af
Natalia Marufuji Aoki <nataliamarufuji@gmail.com>
2017-10-18 11:47:16 -0200

    classe helper

---
+++ client/app/helper/CustomValidator.ts
@@ -0,0 +1,28 @@
+import {AbstractControl} from '@angular/forms';
+
+export class CustomValidation {
+
+    static MatchPassword(AC: AbstractControl) {
+       let password = AC.get('senha').value; // to get value in input tag
+       let confirmPassword = AC.get('confirmaSenha').value; // to get value in input tag
+        if(password != confirmPassword) {
+            console.log('false');
+            AC.get('confirmaSenha').setErrors( {MatchPassword: true} )
+        } else {
+            console.log('true');
+            return null
+        }
+    }
+
+    static EmailValidator(control: AbstractControl) {
+        let email = control.get('email').value;
+        let pattern = "/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/";
+        var regex = new RegExp(pattern);
+
+        if (!regex.test(email)) {
+            console.log('false');
+            control.get('email').setErrors( {EmailValidator: true} )
+        }
+        return null
+    }
+}
